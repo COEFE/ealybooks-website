97d4c7c85a670551f7ab3444fdbeefebef89ad56
Christopher Ealy <chris.ealy@protonmail.com>
2025-12-12 22:59:39 -0500

    Reduce header vertical padding

--- components/header.tsx
+++ components/header.tsx
@@ -17,7 +17,7 @@ export function Header() {
 
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-white border-b-2 border-black">
-      <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+      <div className="max-w-7xl mx-auto px-6 py-2 flex items-center justify-between">
         <Link href="/" className="flex items-center">
           <Image
             src="/logo-final.png"
